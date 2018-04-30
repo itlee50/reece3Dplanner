ebdd3ea39cf67998d83886a74e574ca517f73775
itlee50 <38851197+itlee50@users.noreply.github.com>
2018-04-30 17:29:51 +1000

    Add files via upload

---
+++ drag.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drag : MonoBehaviour
+{
+    float distance = 10;
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+}
