b25a11ddd2a8cafbc891046b50585690c25b2203
itlee50 <38851197+itlee50@users.noreply.github.com>
2018-05-20 15:14:10 +1000

    Add files via upload

---
+++ Rotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public KeyCode pressRight;
+    public KeyCode pressLeft;
+    public KeyCode pressUp;
+    public KeyCode pressDown;
+    public float rotationSpeed = 100.0F;
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(pressRight))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(pressLeft))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(pressUp))
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(pressDown))
+        {
+            transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+        }
+    }
+}
